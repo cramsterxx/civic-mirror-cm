c2a343086e1ce5de036568c82c4d8d6fb559a4bd
cramsterxx <iamsocool3231@gmail.com>
2025-03-28 09:55:25 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,10 @@
+from flask import Flask, jsonify, request
+from routes import init_routes
 
+app = Flask(__name__)
+
+# Initialize routes
+init_routes(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
